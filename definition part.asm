862f2d5de432f7c791413ad7f74740d97b8529f4
yarayousri <66217864+yarayousri@users.noreply.github.com>
2022-12-16 20:41:03 +0200

    Update definition part.asm

--- definition part.asm
+++ definition part.asm
@@ -1,7 +1,6 @@
 
 ;This part of our project is definition of variables ,which we used in our project
 
-
 .MODEL SMALL
 .STACK 100H
 .DATA       
@@ -12,27 +11,27 @@ INTRO DB '************************* Medical Billing Store **********************
 
                               
 
-ENTER DB 10,13,'PLEASE ENTER THE KEYS WHAT YOU WANT TO BUY: $'
+ENTER DB 10,13,'PLEASE ENTER THE CODE WHAT YOU WANT TO BUY: $'
 
-INFO DB 10,13,'KEYS     ITEMS                PRICE$'
+INFO DB 10,13,'Code     Medicines            Price$'
 
-CS_MALE DB 10,13,' 1       Panadol              10EGP $'
+panadol DB 10,13,' 1       Panadol              10EGP $'
                           
-FS_MALE DB 10,13,' 2       Paracetamol          15EGP $'
+Paracetamol DB 10,13,' 2       Paracetamol          15EGP $'
                         
-PANT_MALE DB 10,13,' 3       Cleritek             13EGP $'
+Cleritek DB 10,13,' 3       Cleritek             13EGP $'
 
-M_SHOES DB 10,13,' 4       Aspirin              12EGP $'
+Aspirin DB 10,13,' 4       Aspirin              12EGP $'
 
-CS_FEMALE DB 10,13,' 5       Brufen               19EGP $'
+Brufen DB 10,13,' 5       Brufen               19EGP $'
 
-PANT_FEMALE DB 10,13,' 6       Surbex               20EGP $'
+Surbex DB 10,13,' 6       Surbex               20EGP $'
 
-F_SHOES DB 10,13,' 7       Arinac               16EGP $'
+Arinac DB 10,13,' 7       Arinac               16EGP $'
 
-PANJABI DB 10,13,' 8       Sinopharm Vaccine    22EGP $'
+Sinopharm  DB 10,13,' 8       Sinopharm            22EGP $'
                                                     
-KURTI DB 10,13,' 9       Pfizer Vaccine       28EGP $'
+Pfizer  DB 10,13,' 9       Pfizer               28EGP $'
 
 E_QUANTITY DB 10,13,'ENTER QUANTITY: $'
 
@@ -63,6 +62,5 @@ S DW 0,'$'
                                  
 NL DB 0DH,0AH,'$'                ;NEW LINE
 
-
-
-
+     
+     
